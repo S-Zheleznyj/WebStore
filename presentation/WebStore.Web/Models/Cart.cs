2ac1caac5ec885d59ba72a93eb5850b01cbf9ddc
Sergey Zheleznyy <sergo1.iron@gmail.com>
2022-07-12 09:56:17 +0300

    Добавить книгу в корзину #7

---
+++ presentation/WebStore.Web/Models/Cart.cs
@@ -0,0 +1,8 @@
+﻿namespace WebStore.Web.Models
+{
+    public class Cart
+    {
+        public IDictionary<int, int> Items { get;set; } = new Dictionary<int, int>();
+        public decimal Amount { get; set; }
+    }
+}
